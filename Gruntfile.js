8493fbb47c2df3d30cc100cc99fa3b66754980c1
Rodrigo Muniz <eu@rodrigomuniz.com>
2014-12-08 02:32:44 -0200

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: ['scripts/*'],
+        dest: '_site/scripts/main.js',
+        options: {
+          separator: ';'
+        }
+      }
+    },
+    uglify: {
+      js: {
+        src: 'scripts/main.js',
+        dest: '_site/scripts/main.min.js'
+      }
+    },
+    watch: {
+      js: {
+        files: ['scripts/**/*.js'],
+        tasks: ['default'],
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch', 'concat', 'uglify']);
+};
