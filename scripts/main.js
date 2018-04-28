c51516db75e553ca069b5762e22cd2884c64d6f1
Rodrigo Muniz <eu@rodrigomuniz.com>
2018-04-28 20:03:23 -0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-console.log("opa");
+var carousel = new Carousel();
+carousel
+    .add('Black Music')
+    .add('Brasilidade')
+		.add('Funk')
+		.add('Baile Charme')
+		.add('Hip Hop')
+		.add('Manguebeat')
+		.add('Afrofuturismo')
+    .add('Tropical')
+    .add('Twerk')
+    .add('Afrobeat')
+		.add('Soul')
+    .add('Dancehall')
+    .showFor(1200) // set the time in milliseconds how long a word is shown
+    .onUpdate(
+        function (partial) {
+            document.getElementById('carousel').innerHTML = partial;
+        }
+    ).run(); // start the loop
